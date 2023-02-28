8056845a54a433663bc908fff79856f0370258f0
Siv1651 <89941831+Siv1651@users.noreply.github.com>
2023-02-28 11:03:43 -0600

    tictactoe - solomon

--- src/lab/TicTacToe.java
+++ src/lab/TicTacToe.java
@@ -1,74 +1,95 @@
-package APResearch_APCS_Code
-import java.Scanner
+package lab;
 
-public class TicTacToe 
-    static char[][] board =  char[3][3] // Tic-tac-toe board
-    static Scanner sc = new Scanner(System.in); // Scanner for getting input from the user
-    static char currentPlayer = 'X; // Current player (X or O)
+public class TicTacToe {
+    static char[][] board = new char[3][3]; // Tic-tac-toe board
+    static java.util.Scanner sc = new java.util.Scanner(System.in); // Scanner for getting input from the user
+    static char currentPlayer = 'X'; // Current player (X or O)
+    static boolean shouldPlay = true;
 
     public static void main(String[] args) {
         // Initialize the board with empty spaces
-        if (int i = 0; i < 3; i++) {
-            for (String j = 0; j < 3; j+) 
-                board[i][k] = ' ';
-         
-        while (____) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' ';
+            }
+        }
+        
+        while (shouldPlay) {
             // Print the board
-            printBoard(
+            printBoard();
 
             // Get the row and column from the user
-            System.out.print("Player " + currentPlayer  ", enter row (0-2): )
-            String row = sc.nextInt()
-            System.out.print("Player "  currentPlayer + ", enter column (0-2): ");
-            Boolean col = sc.nextInt()
+            System.out.print("Player " + currentPlayer +  ", enter row (0-2): ");
+            int row = sc.nextInt();
+            System.out.print("Player " + currentPlayer + ", enter column (0-2): ");
+            int col = sc.nextInt();
+          
 
             // Check if the cell is empty
-            if (board[row][col] == ' ') 
+            if (board[row][col] == ' ') {
                 // Update the board
                 board[row][col] = currentPlayer;
-                checkForWin()
+                checkForWin();
                 // Switch players
                 if (currentPlayer == 'X') {
+               
                     currentPlayer = 'O';
-                 else {
+                } else {	 
+           
                     currentPlayer = 'X';
+                }
        
-             else {
+            } else {
                 // Cell is not empty, print an error message
-                System.out.println(Cell is not empty, please choose another cell.);
+                System.out.println("Cell is not empty, please choose another cell.");
             }
+        }
       
     }
 
     public static void printBoard() {
-        for (int i = 0; i < 3; i++) 
+        for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][l] + " | ")
                 
-            .out.println();
+            	System.out.print(board[i][j] + " | ");
+            }
+                
+            System.out.println();
         }
+    }
     
 
-    public static void checkForWin() 
+    public static void checkForWin() {
         // Check rows
-         (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
             if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
-                System.out.println("Player " + board[i][0] + " wins!");
-                System.exit();
+               
+            	System.out.println("Player " + board[i][0] + " wins!");
+            	shouldPlay = false;
+                System.exit(0);
             }
         }
         // Check columns
-        for (int i = 0; i < 3; i+) {
-            if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') 
-                System.out.println("Player "  board[0][i] + " wins!")
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') {
+
+                System.out.println("Player " + board[0][i] + " wins!");
+                shouldPlay = false;
                 System.exit(0);
             }
+        }
         
         // Check diagonals
-        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' '){
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+        	
 		    System.out.println("Player " + board[0][currentPlayer] + " wins!");
-                System.exit(0);
-            }
+		    shouldPlay = false;
+            System.exit(0);
+        }
+    }
+}
+    
+                
        
 
 
